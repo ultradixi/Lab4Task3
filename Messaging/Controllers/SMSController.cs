3c2acaa570827ef49166348c1cf0497dbbb6509d
Diana Oltean <dyana_mysport@yahoo.com>
2021-03-22 15:50:26 +0000

    EAD Lab 4 Task3

---
+++ Messaging/Controllers/SMSController.cs
@@ -0,0 +1,43 @@
+﻿using Messaging.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Messaging.Controllers
+{
+    public class SMSController : Controller
+    {
+
+
+        // GET: SMSController/Details/5
+        public ActionResult Details(TextMessage msg)
+        {
+            return View(msg);
+        }
+
+        // GET: SMSController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: SMSController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(TextMessage sms)
+        {
+            try
+            {
+                return RedirectToAction("Details",sms);
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+    }
+}
---
+++ Messaging/Models/TextMessage.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Messaging.Models
+{
+    public class TextMessage
+    {
+        [Required]
+        [StringLength(10,MinimumLength=10, ErrorMessage="Number must be 10 digtis minimum")]
+        [RegularExpression(@"^\d{10}$", ErrorMessage = "Number must be 10 digtis minimum")]
+        [DisplayName("To")]
+        public string ToNumber { get; set; }
+        [Required]
+        [StringLength(140, ErrorMessage = "140 character max")]
+        [DisplayName("Message")]
+        public string Message { get; set; }
+
+
+    }
+}
